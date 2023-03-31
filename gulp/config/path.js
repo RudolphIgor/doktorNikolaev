3096553925998a20439a8f9179a2617b1f945bf4
Rudolph <azazil47@gmail.com>
2023-03-31 10:41:18 +0700

    Создан проект спозиционирована шапка и первый экран

---
+++ gulp/config/path.js
@@ -0,0 +1,38 @@
+//Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`; //Папка с результатами проекта
+const srcFolder = `./src`; //Папка с исходниками проекта
+
+export const path = {
+	build: {
+		js: `${buildFolder}/js/`,
+		images: `${buildFolder}/img/`,
+		html:`${buildFolder}/`,
+		fonts: `${buildFolder}/fonts/`,
+		files: `${buildFolder}/files/`,
+		css: `${buildFolder}/css/`,
+	},
+	src: {
+		js: `${srcFolder}/js/app.js`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		svg: `${srcFolder}/img/**/*.svg`,
+		html: `${srcFolder}/*.html`,
+		files: `${srcFolder}/files/**/*.*`,
+		scss: `${srcFolder}/scss/style.scss`,
+		svgicons: `${srcFolder}/svgicons/*.svg`
+	},
+	watch: {
+		js: `${srcFolder}/js/**/*.js`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+		html: `${srcFolder}/**/*.html`,
+		files: `${srcFolder}/files/**/*.*`,
+		scss: `${srcFolder}/scss/**/*.scss`,
+	},
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	srcFolder: srcFolder,
+	rootFolder: rootFolder,
+	ftp: `test`
+}
